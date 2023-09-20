0a02b637af37d370c71c1368bcb9cda5be4530f4
Sven Kautlenbach <sven.kautlenbach@cgi.com>
2023-09-20 12:38:50 +0300

    E2E tests run scenario test

---
+++ build-automation/create_exe_in_container.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Simply builds the given CMake project in a container to a directory given as arguments
+
+set -e
+
+if [ "$#" -ne 2 ]; then
+    echo "$0 requires 2 arguments."
+    echo "Usage:"
+    echo "$0 [repo dir where root CMakeLists.txt resides] [docker image]"
+    exit 1
+fi
+
+# Resolve it to full path to avoid problems later.
+repo_dir=$(readlink -m "$1")
+docker_image="$2"
+
+# Check if the path is a directory
+if [ -d "$repo_dir" ]; then
+  # Extract the last folder name
+  repo_folder_name=$(basename "$repo_dir")
+else
+  echo "Given repo dir ${repo_dir} is not a directory or does not exist."
+  exit 1
+fi
+
+# Check if the image name includes a tag (':' character)
+if [[ $docker_image == *":"* ]]; then
+  # Extract image name when tag is supplied
+  image_name=$(echo "$docker_image" | awk -F '/' '{print $NF}' | awk -F ':' '{print $1}')
+else
+  # Extract image name when no tag is supplied
+  image_name=$(echo "$docker_image" | awk -F '/' '{print $NF}')
+fi
+
+
+container_name="${image_name}-${repo_folder_name}"
+container_work_dir="/root/"
+docker run -t -d --name "${container_name}" "${docker_image}"
+docker cp "${repo_dir}" "${container_name}":"${container_work_dir}/"
+container_work_dir_repo="${container_work_dir}/${repo_folder_name}"
+echo "container_work_dir_repo - ${container_work_dir_repo}"
+docker exec -t "${container_name}" bash -c "${container_work_dir_repo}/build-automation/compile_build.sh ${container_work_dir_repo} ${container_work_dir_repo}/build"
+docker exec -t "${container_name}" bash -c "ln -s ${container_work_dir_repo}/build/asar_focus /usr/bin/asar_focus"
+docker stop "${container_name}"
+docker commit "${container_name}" "${container_name}-exe"
+docker rm "${container_name}"
---
+++ build-automation/run_e2e_container.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Simply builds the repository
+
+set -e
+
+if [ "$#" -ne 2 ]; then
+    echo "$0 requires 2 arguments."
+    echo "Usage:"
+    echo "$0 [End-to-end test asset dir] [docker image]"
+    exit 1
+fi
+
+# Resolve it to full path to avoid problems later.
+e2e_dir=$(readlink -m "$1")
+docker_image="$2"
+
+# Check if the path is a directory
+if [ -d "$e2e_dir" ]; then
+  # Extract the last folder name
+  e2e_dir_name=$(basename "$e2e_dir")
+else
+  echo "Given repo dir ${e2e_dir} is not a directory or does not exist."
+  exit 1
+fi
+
+# Check if the image name includes a tag (':' character)
+if [[ $docker_image == *":"* ]]; then
+  # Extract image name when tag is supplied
+  image_name=$(echo "$docker_image" | awk -F '/' '{print $NF}' | awk -F ':' '{print $1}')
+else
+  # Extract image name when no tag is supplied
+  image_name=$(echo "$docker_image" | awk -F '/' '{print $NF}')
+fi
+
+mkdir -p "${e2e_dir}"
+
+container_name="${image_name}_e2e"
+container_work_dir="/root/e2e"
+docker run -t -d --gpus all -v "${e2e_dir}":"${container_work_dir}" --name "${container_name}" "${docker_image}"
+docker exec -t "${container_name}" bash -c "cd /alus/skript && source venv/bin/activate && pip3 install -r requirements.txt && python3 skript.py ${container_work_dir}/skripts/*.yaml"
+docker stop "${container_name}"
+docker rm "${container_name}"
