4af08446992801ec8f447d83a18946f639714788
Sven Kautlenbach <sven.kautlenbach@cgi.com>
2023-11-29 15:06:46 +0200

    ERS packets samples parsing and converting efficient.

--- envisat_format/src/envisat_format_kernels.cu
+++ envisat_format/src/envisat_format_kernels.cu
@@ -13,6 +13,7 @@
 #include "cuda_util/cuda_algorithm.cuh"
 #include "cuda_util/cuda_bit.cuh"
 #include "envisat_types.h"
+#include "ers_env_format.h"
 
 namespace alus::asar::envformat {
 
@@ -60,6 +61,47 @@ void ConditionResults(DevicePaddedImage& img, char* dest_space, size_t record_he
     dim3 grid_sz((x_size_stride + 15) / 16, (y_size + 15) / 16);
     CalibrateClampToBigEndian<<<grid_sz, block_sz>>>(img.Data(), x_size_stride, x_size, y_size, dest_space,
                                                      record_header_size, calibration_constant);
+    CHECK_CUDA_ERR(cudaDeviceSynchronize());
+    CHECK_CUDA_ERR(cudaGetLastError());
+}
+
+__global__ void ConvertErsImSamplesToComplexKernel(uint8_t* samples, int samples_range_size_bytes, int packets,
+                                                   uint16_t* swst_codes, uint16_t min_swst, cufftComplex* target_buf,
+                                                   int target_range_width) {
+    const int x = threadIdx.x + blockIdx.x * blockDim.x;
+    const int y = threadIdx.y + blockIdx.y * blockDim.y;
+    const int x_size = samples_range_size_bytes;
+
+    if (x < x_size && y < packets) {
+        const int buffer_index = y * x_size + x;
+        const int target_buffer_index = y * target_range_width + (x / 2);
+        // ERS SWST multiplier = 4
+        int offset = 4 * (swst_codes[y] - min_swst);
+        if (x % 2 == 0) {
+            target_buf[target_buffer_index + offset].x = static_cast<float>(samples[buffer_index]);
+        } else {
+            target_buf[target_buffer_index + offset].y = static_cast<float>(samples[buffer_index]);
+        }
+    }
+}
+
+void ConvertErsImSamplesToComplex(uint8_t* samples, size_t source_range_samples, size_t packets, uint16_t* swst_codes,
+                                  uint16_t min_swst, cufftComplex* gpu_buffer, size_t target_range_padded_samples) {
+    const auto samples_range_bytes = source_range_samples * 2 * sizeof(uint8_t);
+    const auto samples_total_bytes = packets * samples_range_bytes;
+    uint8_t* samples_gpu;
+    CHECK_CUDA_ERR(cudaMalloc(&samples_gpu, samples_total_bytes));
+    CHECK_CUDA_ERR(cudaMemcpy(samples_gpu, samples, samples_total_bytes, cudaMemcpyHostToDevice));
+    uint16_t* swst_codes_gpu;
+    CHECK_CUDA_ERR(cudaMalloc(&swst_codes_gpu, packets * sizeof(uint16_t)));
+    CHECK_CUDA_ERR(cudaMemcpy(swst_codes_gpu, swst_codes, packets * sizeof(uint16_t), cudaMemcpyHostToDevice));
+
+    const auto x_size = ers::highrate::MEASUREMENT_DATA_SIZE_BYTES;
+    const auto y_size = packets;
+    dim3 block_size(16, 16);
+    dim3 grid_sz((x_size + 15) / 16, (y_size + 15) / 16);
+    ConvertErsImSamplesToComplexKernel<<<grid_sz, block_size>>>(
+        samples_gpu, samples_range_bytes, packets, swst_codes_gpu, min_swst, gpu_buffer, target_range_padded_samples);
     CHECK_CUDA_ERR(cudaDeviceSynchronize());
     CHECK_CUDA_ERR(cudaGetLastError());
 }
